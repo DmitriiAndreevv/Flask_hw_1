a99feb9457ea5cb7bcfc8a8231065da399ab9afd
DMITRII ANDREEV <111002352+DmitriiAndreevv@users.noreply.github.com>
2024-01-25 22:06:46 +0300

    Add files via upload

---
+++ Flask_hw_2/registr.py
@@ -0,0 +1,56 @@
+from flask import Flask, make_response, redirect, render_template, request, url_for,flash
+from markupsafe import escape
+
+app = Flask(__name__)
+app.secret_key = b'f57f3a0bd0192d415028b4394165b34dbc95a97f1447aae754d9939180fe3db5'
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/cookie/', methods=['POST'])
+def cookie():
+    username = request.form['name']
+    usermail = request.form['mail']
+
+    response = make_response(redirect('/hello/'))
+    response.set_cookie('name', username)
+    response.set_cookie('mail', usermail)
+    return response
+
+
+@app.route('/hello/')
+def hello():
+    username = request.cookies.get('name')
+    usermail = request.cookies.get('mail')
+    if not username or not usermail:
+        flash('Введите имя и почту!', 'danger')
+        return redirect(url_for('login'))
+    flash('Форма успешно отправлена', 'success')
+    return render_template('hello.html', name=username)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    context = {
+        'task': 'Registr'
+    }
+    if request.method == 'POST':
+        username = request.form.get('name')
+        usermail = request.form.get('mail')
+        context = {'username': username,
+                   'usermail': usermail}
+    return render_template('login.html', **context)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    response = make_response(redirect('/'))
+    response.delete_cookie('name')
+    response.delete_cookie('mail')
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
